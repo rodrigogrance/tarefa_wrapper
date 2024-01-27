329dc1304ea5d37c1878988f4ae2dd344a3eed33
rodrigogrance <143683364+rodrigogrance@users.noreply.github.com>
2024-01-27 00:25:20 -0300

    Initial commit

---
+++ src/br/com/rlacorte/Wrapper.java
@@ -0,0 +1,12 @@
+package br.com.rlacorte;
+
+public class Wrapper {
+
+    public static void main(String[] args) {
+
+        int idade = 10;
+        System.out.println(idade);
+        Integer idadeI = 100;
+        System.out.println(idadeI);
+    }
+}
